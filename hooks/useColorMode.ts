90bf5221641e7e86482bd637f6ee6aea985c90d8
jaieosom <jaieosom@gmail.com>
2022-05-16 17:29:07 -0300

    first commit material UI next

---
+++ hooks/useColorMode.ts
@@ -0,0 +1,7 @@
+import { useContext } from "react";
+import ColorModeContext from "../contexts/ColorModeContext";
+
+
+const useColorMode = () => useContext(ColorModeContext);
+
+export default useColorMode;
